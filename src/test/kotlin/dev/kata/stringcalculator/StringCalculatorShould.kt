19494a6e69ff5a2c4f05333067748d8c57157075
Miguel Ángel Pérez García <miguel.perez@tii.ae>
2023-12-01 13:14:16 +0000

    fix: green test for string calculator should throw not implemented

--- src/test/kotlin/dev/kata/stringcalculator/StringCalculatorShould.kt
+++ src/test/kotlin/dev/kata/stringcalculator/StringCalculatorShould.kt
@@ -2,21 +2,18 @@ package dev.kata.stringcalculator
 
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.Test
-import org.mockito.kotlin.doThrow
-import org.mockito.kotlin.mock
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when`
 
 internal class StringCalculatorShould {
     @Test
     fun `throw not implemented`() {
-
-        val dependencyMock = mock<StringCalculatorDependency> {
-            on { doSomething() }
-            doThrow(NotImplementedError("Time to do some TDD..."))
-        }
+        val dependency = mock(StringCalculatorDependency::class.java)
+        `when`(dependency.doSomething()).thenThrow(NotImplementedError("Time to do some TDD..."))
 
         assertThatExceptionOfType(NotImplementedError::class.java)
             .isThrownBy {
-                StringCalculator(dependencyMock).execute()
+                StringCalculator(dependency).execute()
             }.withMessage("Time to do some TDD...")
     }
 }
